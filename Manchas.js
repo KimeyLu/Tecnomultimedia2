4a0958613802512b32519110917a48424affaa46
KimeyLu <128751189+KimeyLu@users.noreply.github.com>
2024-06-26 18:20:22 -0300

    v3.2

--- Manchas.js
+++ Manchas.js
@@ -1,15 +1,55 @@
 class Manchas {
   constructor() {
-    this.manchasB = loadImage('data/mancha01.png');
-    /*let imagesOnScreen = [];
-    let cantidadMancha = 4;*/
+    //let img;
+    this.manchasB = [];
+    //let imagesOnScreen = [];
+    this.cantidadMancha = 4;
     
-    //this.manchasB = loadImage('data/mancha01.png');
+    /*this.x = random(0, width);
+    this.y = random(0, height);
+    this.x2 = random(100, 200);
+    this.y2 = random(100, 200);*/
+    
+    
+    this.x = this.x;
+    
+    this.y = 0;
+    this.x2 = 0;
+    this.y2 = 0;
+    
+    //img = loadImage('data/fondo.png');
+    
+    for(let i = 0; i < this.cantidadMancha; i++) {
+      let nombre = "data/mancha" + nf(i, 2) + ".png";
+      this.manchasB[i] = loadImage(nombre);
+    }
   }
   
   dibujar() {
-      // Dibujar las imágenes de manchas
-      image(this.manchasB, 0, 0, width, height);
+      // Dibujar las imágenes de manchas   
+    /*this.x = random(0, width);
+    this.y = random(0, height);
+    this.x2 = random(100, 200);
+    this.y2 = random(100, 200);*/
+
+  if (estado == "fondo") {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.x2 = random(100, 200);
+    this.y2 = random(100, 200);
   }
+  
+    for(let i = 0; i < this.cantidadMancha; i++) {
+       push();
+       tint(255, 100);
+       imageMode(CENTER);
+       image(this.manchasB[i],this.x, this.y, this.x2, this.y2);
+       pop();
+    }
+  }
+  
+  
+  
+  
   
 }
