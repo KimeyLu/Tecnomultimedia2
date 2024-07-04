8149f7a514936778af9577ea1b8b2c325b6b0ac7
Ariana08 <103059917+Ariana08@users.noreply.github.com>
2024-07-04 10:19:52 -0300

    Add files via upload

---
+++ Tecnomultimedia2-estadomanchas/Manchas.js
@@ -0,0 +1,127 @@
+class Manchas {
+  constructor() {
+    //variables para condiciones en aparecer (quizas poner "this.")
+    this.mostrarIzquierdaSuperior = false;
+    this.mostrarDerechaSuperior = false;
+    this.mostrarIzquierdaInferior = false;
+    this.mostrarDerechaInferior = false;
+    
+    
+    //dibujar imagenes
+    this.tamañoImagen = 250;
+    this.imgIzquierda = loadImage('data/mancha00.png');
+    this.imgDerecha = loadImage('data/mancha02.png');
+      //superior
+      this.posXIzquierdaSup = width - this.tamañoImagen; 
+      this.posYIzquierdaSup = 0;
+      this.posXDerechaSup = 0; 
+      this.posYDerechaSup = 0;
+      //inferior
+      this.posXIzquierdaInf = 0; 
+      this.posYIzquierdaInf = height - this.tamañoImagen;
+      this.posXDerechaInf = width - this.tamañoImagen; 
+      this.posYDerechaInf = height - this.tamañoImagen;
+     
+  }
+  
+  aparecer(){
+   // if (!this.mostrarDerechaSuperior && !this.mostrarIzquierdaSuperior && !this.mostrarIzquierdaInferior && !this.mostrarDerechaInferior) {
+      this.mostrarDerechaSuperior = true;  //ahora, si pongo true, hay 2 que se dibujan al mismo tiempo
+   // }
+    if (this.mostrarDerechaSuperior && !this.mostrarIzquierdaSuperior) {
+      this.mostrarIzquierdaSuperior = true;
+    } else if (this.mostrarIzquierdaSuperior && !this.mostrarIzquierdaInferior) {
+      this.mostrarIzquierdaInferior = true;
+    } else if (this.mostrarIzquierdaInferior && !this.mostrarDerechaInferior) {
+      this.mostrarDerechaInferior = true;
+    }
+  }
+  
+  dibujar(){
+    this.imgIzquierda.resize(this.tamañoImagen, 0);
+    this.imgDerecha.resize(this.tamañoImagen, 0);
+    
+    if (this.mostrarIzquierdaSuperior) {
+      image(this.imgIzquierda, this.posXIzquierdaSup, this.posYIzquierdaSup); //no es izquierda, es derecha, solucionar despues
+    }
+     //otras imagenes 
+    if (this.mostrarDerechaSuperior) {
+      image(this.imgDerecha, this.posXDerechaSup, this.posYDerechaSup);
+    }
+    if (this.mostrarIzquierdaInferior) {
+      image(this.imgIzquierda, this.posXIzquierdaInf, this.posYIzquierdaInf);
+    }
+    if (this.mostrarDerechaInferior) {
+      image(this.imgDerecha, this.posXDerechaInf, this.posYDerechaInf);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Manchas {
+  constructor() {
+    //nuevo
+    this.manchasB = [];   
+    this.x = this.x;  
+    this.y = 0;
+    this.x2 = 0;
+    this.y2 = 0;   
+    for(let i = 0; i < 4; i++) {
+      let nombre = "data/mancha" + nf(i, 2) + ".png";
+      this.manchasB[i] = loadImage(nombre);
+    }  
+    this.historial = [];    
+  }
+  
+  dibujar() { 
+   background(207,193,166);
+   //tint(255, 100);
+      if (estado == "fondo") {
+        if (haySonido){
+          this.x = random(width);
+          this.y = random(height);
+          this.x2 = 200;
+          this.y2 = 200;
+        }
+      } else {
+        this.x = this.x; 
+        this.y = this.y; 
+        this.x2 =this.x2 ;
+        this.y2 = this.y2;
+      }
+       
+       var vectorPosicion = createVector(this.x, this.y);
+       this.historial.push(vectorPosicion);
+       //console.log(this.historial);     
+  
+       for(let j = 0; j < 4; j++) {
+         for(let i = 0; i < this.historial.length; i++) {
+           var pos = this.historial[i];
+           push();
+             //tint(255, 100);  // si pongo todo esto junto el programa se lageea 
+             imageMode(CENTER);  
+             image(this.manchasB[j],pos.x, pos.y, this.x2, this.y2);
+           pop();
+         }
+       }
+  }
+}*/
---
+++ Tecnomultimedia2-estadomanchas/estadosSonidoSinClasescopia.js
@@ -0,0 +1,414 @@
+let monitorear = false;
+
+//otro canvas
+//let otroCanvas;
+let capaFrente;
+let capaDelMedio;
+
+//ml5
+let mic;
+let pitch;
+let audioContext;
+
+//gestor
+let gestorAmp;
+let gestorPitch;
+
+//------------FREC Y AMP (Calibrable)----------------
+let FREC_MIN = 49;
+let FREC_MAX = 250;
+let AMP_MIN = 0.0000001;
+let AMP_MAX = 0.05;
+//---------------------------------------------------
+//eventos
+let haySonido;
+let antesHabiaSonido;
+
+//estados
+let estado = "fondo";
+//let estado = "fondo";
+
+let marca;
+let tiempoLimitefondo = 1000; //5secs (despues de hacer ruido)
+let tiempoLimitebarra = 1000; //10secs (despues de hacer ruido o si no hago ruido)
+let tiempoLimiteplumas = 1000; //10secs (despues de hacer ruido o si no hago ruido)
+let tiempoLimitereiniciar = 1000; //5secs (despues de hacer ruido o si no hago ruido)
+
+const model_url = 'https://cdn.jsdelivr.net/gh/ml5js/ml5-data-and-models/models/pitch-detection/crepe/';
+//canvas tamaños random
+let windowsX;
+let windowsY;
+
+//---VARIABLES DE LAS MANCHAS---------------------------------------------------------------------//
+let manchas;
+let img;
+let manchasB = [];
+//let imagesOnScreen = [];
+let cantidadMancha = 4; // Número de imágenes de manchas
+///nuevo
+let xmanchas;
+let ymanchas;
+let x2manchas;
+let y2manchas;
+
+
+
+//---VARIABLES DE LA BARRA---------------------------------------------------------------------//
+let ampli;
+let tamañoMaxRec;
+
+let primerColorGrad, segundoColorGrad;
+
+let colorBarraEstatica;
+let tamañoBarra;
+let tamañoMinimoBarra;
+let posRandomY;
+//---VARIABLES DE LAS PLUMAS---------------------------------------------------------------------//
+let imagenes = [];
+let posicionesX = [];
+let direcciones = [];
+let angulos = [];
+let imagenesGrupo1 = [];
+let imagenesGrupo2 = [];
+let imagenesGrupo3 = [];
+let posicionesXGrupo1 = [];
+let posicionesYGrupo1 = [];
+let posicionesXGrupo2 = [];
+let posicionesYGrupo2 = [];
+let posicionesXGrupo3 = [];
+let posicionesYGrupo3 = [];
+//let fondo;
+// colocar un falce y un true por tiempo para la rotacion timelaps o colocar si hay mucho ruido se activa la rotacion
+//hasta que deje de haber sonido
+let rotacionActivada = false;
+let gradosRotacion = 0;
+let anguloObjetivo = 190;
+// cambiar mejor el sonido
+/*let AMP_MIN = 0.0000001;
+let AMP_MAX = 0.06;*/
+let AMP_MED = 0.3;
+let amp;
+let haySonidomanchas = false;
+let MuchoSonido = false;
+let PosY = 178;
+let fondoPosX = 0;
+let fondoPosY = 0;
+
+
+let imgaen;
+
+function preload() {
+  img = loadImage('data/fondo.png');
+  //-------------------Plumas-------------------------/
+    for(let i = 0; i < cantidadMancha; i++) {
+      let nombre = "data/mancha" + nf(i, 2) + ".png";
+      manchasB[i] = loadImage(nombre);
+    }
+
+  //-------------------Plumas-------------------------/
+  imagenes[0] = loadImage("data/obj1.png");
+  imagenes[1] = loadImage("data/obj2.png");
+  imagenes[2] = loadImage("data/obj3.png");
+  
+  //prueba
+  imgaen = loadImage("data/fondo_tecno.png");
+}
+
+function setup() {
+  windowsX = random(600, 700);
+  windowsY = random(400, 500);
+  createCanvas(windowsX, windowsY);
+  background(207,193,166);
+  otroCanvas = createGraphics(windowsX, windowsY);
+  //otroCanvas = createImage(windowsX, windowsY);
+  //capaDelMedio = createGraphics(windowsX, windowsY);
+  //otroCanvas.clear();
+    manchas = new Manchas();
+  
+  audioContext = getAudioContext();
+  mic = new p5.AudioIn();
+  mic.start(startPitch);  
+  userStartAudio();
+  
+  gestorAmp = new GestorSenial(AMP_MIN, AMP_MAX);
+  gestorPitch = new GestorSenial(FREC_MIN, FREC_MAX);  
+  antesHabiaSonido = false;
+  
+  //--manchas--//
+    let xmanchas = 0;
+    let ymanchas = 0;
+    let x2manchas = 0;
+    let y2manchas = 0;
+    
+  //--Barra--//
+  //primerColorGrad = color(0);
+ // segundoColorGrad = color(255, 0, 0);
+
+ /* for(let x = 0; x < width; x++){
+    rango = map(x, 0, width, 0, 1);
+    let gradiente = lerpColor(primerColorGrad, segundoColorGrad, rango);
+    line(0, x, width, x);
+  }*/
+  tamañoMaxRec = windowsX-40;
+  posRandomY = random(height/2, height/3);
+
+  //--plumas--//
+  posicionesX[0] = 130;
+  posicionesX[1] = 180;
+  posicionesX[2] = 230;
+  posicionesX[3] = 280;
+
+  direcciones[0] = 1;
+  direcciones[1] = 1;
+  direcciones[2] = -1;
+  direcciones[3] = -1;
+
+  angulos[0] = 0;
+  angulos[1] = 0;
+  angulos[2] = 0;
+  angulos[3] = 0;
+
+  inicializarPosiciones(imagenesGrupo1, posicionesXGrupo1, posicionesYGrupo1);
+  inicializarPosiciones(imagenesGrupo2, posicionesXGrupo2, posicionesYGrupo2);
+  inicializarPosiciones(imagenesGrupo3, posicionesXGrupo3, posicionesYGrupo3);
+  
+  noStroke();
+}
+
+
+function draw() {
+  ampli = gestorAmp.filtrada; 
+  var vol = mic.getLevel();
+  gestorAmp.actualizar(vol);
+  //console.log("amplitud: "+vol); //Para ver AMP
+  //---------------UMBRAL DE SONIDO (Calibrable)-----------------
+  haySonido = gestorAmp.filtrada > 0.1;
+  //-------------------------------------------------------------
+  console.log("Hay sonido: "+haySonido); ////Para ver umbral
+
+  let inicioElSonido = haySonido && !antesHabiaSonido;
+  let finDelSonido = !haySonido && antesHabiaSonido;
+
+
+  
+  //**************ESTADOS****************
+  if (estado === "fondo") {      
+     //background(207,193,166); 
+     background(206,204,189);  
+     
+     if (inicioElSonido) { //EVENTO
+     manchas.aparecer();
+     manchas.dibujar();
+    } 
+    if (haySonido) {
+      manchas.dibujar();
+    }    
+    
+    //CONTADOR(cambio de estado)-----------//
+      if (finDelSonido) {marca = millis();}
+      if (!haySonido ) {
+        manchas.dibujar();
+        let ahora = millis();
+        if (ahora > marca + tiempoLimitefondo) {estado = "barra";
+          marca = millis();}}
+    //-------------------------------------//
+  } else if (estado === "barra") {   
+   background(206,204,189);       
+   manchas.dibujar(); 
+    
+    //posRandomY = random(height/2, height/3);
+    
+    // Asegurarse de que la Barra negra siempre esté al menos un 70% llena
+    tamañoMinimoBarra = 0.7 * tamañoMaxRec;
+    tamañoBarra = map(ampli, 0, 1, tamañoMinimoBarra, tamañoMaxRec); // Mapear la amplitud filtrada al tamaño de la Barra
+    tamañoBarra = min(tamañoBarra, tamañoMaxRec); // Limitar el tamaño de la barra dinámica al tamaño máximo de la barra estática
+
+    // Mapear el valor del tono a un rango de colores de rojo a blanco            //gestorPitch.actualizar
+     colorBarraEstatica = lerpColor(color(255, 0, 0), color(255, 255, 255), map(pitchValue, 50, 500, 0, 1));
+
+    // Dibujar la Barra estática detrás
+    push();
+    //quizas puedo poner un gradiente aca
+    fill(colorBarraEstatica); // Color de la Barra estática (cambia de rojo a blanco según el tono)
+    rect(20, posRandomY - 25, tamañoMaxRec, 50); // Dibujar la Barra estática
+    pop();
+      //height/2
+      //o aca
+    
+    // Dibujar la Barra dinámica delante (negra)
+    push();
+    fill(0, 0, 0); // Color de la Barra dinámica (negro)
+    rect(20, posRandomY - 25, tamañoBarra, 50); // Dibujar la Barra dinámica
+    pop();
+
+
+    //CONTADOR(cambio de estado)-----------//
+      if (finDelSonido) {marca = millis();}
+      if (!haySonido ) {let ahora = millis();
+        if (ahora > marca + tiempoLimitebarra) {estado = "plumas";
+          marca = millis();}}
+    //-------------------------------------//
+  } else if (estado === "plumas") {
+    background(206,204,189); 
+    manchas.dibujar();
+    
+    posRandomY = posRandomY;
+      
+    // Dibujar la Barra estática detrás
+    push();
+    fill(colorBarraEstatica); // Color de la barra estática (cambia de rojo a blanco según el tono)
+    rect(20, posRandomY - 25, tamañoMaxRec, 50); // Dibujar la barra estática 
+    pop();
+
+    // Dibujar la Barra dinámica delante (negra)
+    push();
+    fill(0, 0, 0); // Color de la Barra dinámica (negro)
+    rect(20, posRandomY - 25, tamañoBarra, 50); // Dibujar la Barra dinámica
+    pop();
+
+ 
+    //   
+      otroCanvas.clear();
+    
+    //---Manchas---//   
+    for (let i = 0; i < imagenes.length; i++) {
+     otroCanvas.push(); //ESTO funciona (casi)
+    //push(); 
+    //otroCanvas.clear();
+     otroCanvas.translate(posicionesX[i], PosY); //maneja el movimiento (de derecha a izquierda)
+    
+     otroCanvas.rotate(angulos[i]); //maneja la rotacion
+    
+     otroCanvas.imageMode(CENTER);      
+
+     otroCanvas.image(imagenes[i], 0, 0, 80, 350);
+    //pop();
+     otroCanvas.pop(); //ESTO funciona (casi)
+//otroCanvas.clear();
+    
+    if (rotacionActivada) {
+      angulos[i] = radians(gradosRotacion);
+    }
+
+    if (haySonidomanchas && !MuchoSonido) {
+      posicionesX[i] += 10 * direcciones[i];
+//cambiar numeros
+      if (posicionesX[i] >= 300 || posicionesX[i] <= 100) {
+        direcciones[i] *= -1;
+      }
+    }
+  }
+  //otroCanvas.clear();
+  //otroCanvas.image(imgaen, 0, 0, 80, 350);
+  image(otroCanvas, 0, 0); 
+
+  amp = mic.getLevel();
+  MuchoSonido = amp > AMP_MAX;
+//rotaccion de angulo cambiar angulo de obj = rotacion
+  if (MuchoSonido && !rotacionActivada) {
+    rotacionActivada = true;
+    if (gradosRotacion === 0) {
+      anguloObjetivo = 70;
+    } else {
+      anguloObjetivo = 10;
+    }
+  }
+
+  haySonidomanchas = amp > AMP_MIN;
+
+  if (rotacionActivada) {
+    if (gradosRotacion < anguloObjetivo) {
+      gradosRotacion++;
+    } else if (gradosRotacion > anguloObjetivo) {
+      gradosRotacion--;
+    } else {
+      //si pones true se queda ahi no rota mas poner para que siga rotando
+      rotacionActivada = false;
+    }
+  }
+
+    //CONTADOR(cambio de estado)-----------//
+      if (finDelSonido) {marca = millis();}
+      if (!haySonido ) {let ahora = millis();
+        if (ahora > marca + tiempoLimiteplumas) {estado = "reiniciar";
+          marca = millis();}}
+    //-------------------------------------//   
+
+  } 
+  if (estado === "reiniciar") {  
+  } 
+  
+  
+  
+
+  
+  //---MONITOREAR ESTADOS---:
+  console.log(estado);
+  /*if(monitorear) {
+    gestorAmp.dibujar(100, 100);
+    gestorPitch.dibujar(100, 200);}*/
+  //console.log("inicio del sonido: "+inicioElSonido);
+  //console.log("fin del sonido: "+finDelSonido);
+  //console.log("Antes habia sonido: "+antesHabiaSonido);
+  //console.log("TIEMPO: "+round((millis())/1000)+"s");
+  //------------------------
+  antesHabiaSonido = haySonido; 
+    
+
+}
+
+//---------------------------------funciones plumas-----//
+function inicializarPosiciones(imagenes, posicionesX, posicionesY) {
+  for (let i = 0; i < imagenes.length; i++) {
+    posicionesX[i] = random(0,400);
+    posicionesY[i] = random(0,400);
+  }
+}
+
+function aparecerManchas(imagenes, posicionesX, posicionesY) {
+  for (let i = 0; i < imagenes.length; i++) {
+    image(imagenes[i], posicionesX[i], posicionesY[i]);
+
+
+  }
+}
+
+//*----------------------------------Key Pressed-----------------------------------*//
+/*function keyPressed() {
+  if(estado == "fondo"){
+    if (key === 'b') {
+      for (let i = 0; i < cantidadMancha; i++) {
+        let currentImage = {
+          img: manchasB[i],
+          x: random(width),
+          y: random(height),
+          width: 100, // Ajusta el tamaño de las manchas según tus necesidades
+          height: 100, // Ajusta el tamaño de las manchas según tus necesidades
+          rotation: 0,
+          tint: color(255, 255, 255)
+        };
+        imagesOnScreen.push(currentImage);
+      }
+    }
+  } else {}
+}*/
+
+//*------------------------------------PITCH---------------------------------------*//
+function startPitch() {
+  pitch = ml5.pitchDetection(model_url, audioContext , mic.stream, modelLoaded);
+}
+
+function modelLoaded() {
+  getPitch();
+}
+
+function getPitch() {
+  pitch.getPitch(function(err, frequency) {
+    if (frequency) {
+      gestorPitch.actualizar (frequency);
+      pitchValue = frequency;
+     // console.log("frec: "+frequency);  //Para ver FREC
+    }
+    getPitch();
+  })
+}
